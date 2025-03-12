8cbea70678b7f50b9a2cd552dc9865d1ff41d850
TryLuthfi <ty01091990@gmail.com>
2025-03-13 02:21:25 +0700

    add Stock Opname view for managing area dashboard with CRUD functionality

---
+++ application/views/StockOpname/sokota.php
@@ -0,0 +1,184 @@
+<?php
+$status = $this->session->flashdata('status');
+$error_log = $this->session->flashdata('error_log');
+
+$total = 1;
+?>
+
+<div class="content-wrapper">
+
+    <section class="content">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark"><?= $judul ?></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Info boxes -->
+                <div class="row">
+                    <!-- fix for small devices only -->
+                    <div class="clearfix hidden-md-up"></div>
+
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <h3 class="card-title">List Area Dashboard </h3>
+                                    </div>
+                                    <div class="col-6">
+                                        <a href="#" class="btn btn-success float-right text-bold"
+                                            data-target="#modal-lg-tambah-periode" data-toggle="modal">Tambah Periode
+                                            &nbsp;<i class="fas fa-plus"></i> </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body table-scrollable">
+                                <table id="tabel_pemasukan" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Periode</th>
+                                            <th>Lokasi Gudang</th>
+                                            <th>Status</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        foreach ($getSOKota as $data):
+                                            ?>
+                                            <tr>
+                                                <td><?= $total++ ?></td>
+                                                <td><?= $data['sop_bulan'] . " - " . $data['sop_tahun'] ?></td>
+                                                <td><?= $data['kota_lokasi_gudang'] ?></td>
+                                                <td><?php if ($data['sop_status'] != '') {
+                                                    echo $data['sop_status'];
+                                                } else {
+                                                    echo 'NOT YET';
+                                                } ?>
+                                                </td>
+                                                <td>
+                                                    <?php if ($data['id_so_kota'] != '') { ?>
+                                                        <a href="<?php echo site_url('StockOpname/SOPeriode/' . $data['id_lokasi_gudang']); ?>"
+                                                            class="btn btn-success btn-sm">
+                                                            Detail &nbsp;<i class="fas fa-share"></i>
+                                                        </a>
+                                                        <a href="<?php echo site_url('StockOpname/hapusKota/' . $data['id_lokasi_gudang']); ?>"
+                                                            class="btn btn-danger btn-sm">
+                                                            Delete &nbsp;<i class="fas fa-trash"></i></a>
+                                                        <a href="#" class="btn btn-warning btn-sm"
+                                                            data-target="#modal-lg-edit<?= $data['id_so_kota'] ?>"
+                                                            data-toggle="modal"> Edit &nbsp; <i class="fas fa-edit"></i></a>
+                                                    <?php } else { ?>
+                                                        <a href="<?= base_url('StockOpname/periode/' . $id_sop . '/lokasi/' . $data['id_lokasi_gudang']); ?>"
+                                                            class="btn btn-success btn-sm">
+                                                            Tambah SO &nbsp;<i class="fas fa-share"></i>
+                                                        </a>
+                                                    <?php } ?>
+                                                </td>
+                                            </tr>
+
+                                        <?php endforeach; ?>
+
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th colspan="1">Total</th>
+                                            <th colspan="2"><?= number_format($total - 1, 0, ',', '.') ?></th>
+                                            <th colspan="4"></th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                    </div>
+                </div>
+        </section>
+
+</div>
+<!-- /.content-wrapper -->
+
+<?php $this->session->set_flashdata('status', 'kosong'); ?>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+
+<script>
+    $(function () {
+
+        // notifikasi allert sukses atau tidak
+        <?php if ($status == 'sukses_tambah') { ?>
+            swal("Success!", "Berhasil Ditambah!", "success");
+        <?php } else if ($status == 'sukses_hapus') { ?>
+                swal("Success!", "Berhasil Dihapus!", "success");
+        <?php } else if ($status == 'sukses_edit') { ?>
+                    swal("Success!", "Berhasil Edit Data!", "success");
+        <?php } else if ($status == 'gagal_tambah') { ?>
+                        swal("Gagal!", "Gagal Menambah Data!", "warning");
+        <?php } else if ($status == 'gagal_edit') { ?>
+                            swal("Gagal!", "Gagal Mengedit Data!", "warning");
+        <?php } else if ($status == 'gagal_hapus') { ?>
+                                swal("Gagal!", "Gagal Menghapus Data!", "warning");
+        <?php } else { ?>
+        <?php } ?>
+    })
+
+</script>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/progressbar.js@1.1.0/dist/progressbar.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+<script src="<?= base_url('assets') ?>/plugins/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets') ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('assets') ?>/plugins/chart.js/Chart.min.js"></script>
+<script src="<?= base_url('assets') ?>/dist/js/demo.js"></script>
+<script src="<?= base_url('assets') ?>/dist/js/pages/dashboardlistarea.js"></script>
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/dist/css/adminlte.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/daterangepicker/daterangepicker.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/select2/css/select2.min.css">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/jquery/jquery.min.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/jquery-ui/jquery-ui.min.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/bootstrap/js/bootstrap.bundle.min.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/chart.js/Chart.min.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/sparklines/sparkline.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/jqvmap/jquery.vmap.min.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/jqvmap/maps/jquery.vmap.usa.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/jquery-knob/jquery.knob.min.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/moment/moment.min.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/daterangepicker/daterangepicker.js">
+<link rel="stylesheet"
+    href="<?= base_url('assets') ?>/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/summernote/summernote-bs4.min.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/dist/js/demo.js">
+<link rel="stylesheet" href="<?= base_url('assets') ?>/dist/js/pages/dashboard.js">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
