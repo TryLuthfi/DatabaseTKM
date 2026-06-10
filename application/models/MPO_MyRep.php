18f9933ebe10637b7234408aeb1c1e9a421c82e2
TryLuthfi <42312913+TryLuthfi@users.noreply.github.com>
2026-06-10 08:52:49 +0700

    feat: Add prioritization for ATP waiting PIC in EMR target processing

--- application/models/MPO_MyRep.php
+++ application/models/MPO_MyRep.php
@@ -2317,6 +2317,10 @@ class MPO_MyRep extends CI_Model
             return $this->resolvePicFromClusterAndAtpStage($row);
         }
 
+        if ($this->shouldPrioritizeAtpWaitingPic($row)) {
+            return 'EMR NRO';
+        }
+
         $uploaded = (int) ($state['uploaded'] ?? 0);
         $approved = (int) ($state['approved'] ?? 0);
         $astriApproved = (int) ($state['astri_approved'] ?? 0);
@@ -2373,6 +2377,17 @@ class MPO_MyRep extends CI_Model
         }
 
         return 'AREA';
+    }
+
+    private function shouldPrioritizeAtpWaitingPic(array $row)
+    {
+        $displayStatus = $this->resolveClusterDetailedStatus($row);
+        if (in_array($displayStatus, ['WAITING JADWAL', 'WAITING JADWAL ATP', 'WAITING ATP', 'PROSES ATP', 'ON PROSES ATP'], true)) {
+            return true;
+        }
+
+        $stageAtp = strtoupper(trim((string) ($row['stage_atp'] ?? '')));
+        return in_array($stageAtp, ['WAITING JADWAL', 'WAITING JADWAL ATP', 'WAITING ATP', 'PROSES ATP', 'ON PROSES ATP'], true);
     }
 
     public function getEmrTargetDetailedStatusLabel(array $row)
