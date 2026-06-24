8bb37b62087ecfe4093488a3df6444a5d9f67b64
TryLuthfi <42312913+TryLuthfi@users.noreply.github.com>
2026-06-24 11:39:26 +0700

    feat: improve PO value calculation by ensuring fallback to original po_value if base value is not available

--- application/models/MPO_MyRep.php
+++ application/models/MPO_MyRep.php
@@ -1070,7 +1070,7 @@ class MPO_MyRep extends CI_Model
                     'po_category' => (string) ($row['po_category'] ?? ''),
                     'on_target' => (int) ($row['on_target'] ?? 0),
                     'po_date' => (string) ($row['po_date'] ?? ''),
-                    'po_value' => (float) ($row['po_value_base'] ?? 0),
+                    'po_value' => (float) ($row['po_value_base'] ?? ($row['po_value'] ?? 0)),
                     'po_value_final' => (float) ($row['po_value_final'] ?? 0),
                     'outstanding_total' => (float) ($row['outstanding_total'] ?? 0),
                     'total_invoiced' => (float) ($row['total_invoiced'] ?? 0),
